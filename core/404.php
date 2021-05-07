4b7510ecd4556061a68fd5989e61b68268a4b4f5
Lucas <83518257+lu700@users.noreply.github.com>
2021-05-07 16:14:08 -0300

    Create 404.php

---
+++ core/404.php
@@ -0,0 +1,7 @@
+<?php include "config/general.php";
+$pub=True;
+$webtitle="404 | ".$WEBSITE_NAME;
+include "core/includes/header.php";
+echo "<h1>Page not found</h1>";
+include "core/includes/footer.php";
+die(); ?>
